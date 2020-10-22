3ac430450660bcd137d8c52b7a2af5bc1a417729
NashTheCoder <naijeria@gmail.com>
2020-10-22 13:23:22 +0300

    added images and component folders and sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+
+        {/* Twitter icon */}
+
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+        {/* SidebarOption */}
+
+        {/* Button -> Tweet */}
+            
+        </div>
+    )
+}
+
+export default Sidebar
