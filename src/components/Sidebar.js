b2fda2465177554ef195b59b340f8a77893cd587
NashTheCoder <naijeria@gmail.com>
2020-10-22 16:09:17 +0300

    added sidebarOptions icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import './Sidebar.css'
-import TwitterIcon from '@material-ui/icons/Twitter'
-import SidebarOption from './SidebarOption'
+import React from 'react';
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import TurnedInNotIcon from '@material-ui/icons/TurnedInNot';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
@@ -11,14 +17,14 @@ function Sidebar() {
         <TwitterIcon />
 
     
-        <SidebarOption text='Home'/>
-        <SidebarOption text='Explore'/>
-        <SidebarOption text='Notifications'/>
-        <SidebarOption text='Messages'/>
-        <SidebarOption text='Bookmarks'/>
-        <SidebarOption text='List'/>
-        <SidebarOption text='Profile'/>
-        <SidebarOption text='More'/>
+        <SidebarOption Icon={HomeIcon} text='Home'/>
+        <SidebarOption Icon={SearchIcon} text='Explore'/>
+        <SidebarOption Icon={NotificationsNoneIcon} text='Notifications'/>
+        <SidebarOption Icon={MailOutlineIcon} text='Messages'/>
+        <SidebarOption Icon={TurnedInNotIcon} text='Bookmarks'/>
+        <SidebarOption Icon={HomeIcon} text='List'/>
+        <SidebarOption Icon={PersonOutlineIcon} text='Profile'/>
+        <SidebarOption Icon={HomeIcon} text='More'/>
 
         {/* Button -> Tweet */}
             
