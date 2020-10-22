a34c2cd5f12ff7d10d2f1ae2735a32ec0cc0955d
NashTheCoder <naijeria@gmail.com>
2020-10-22 13:16:31 +0300

    created react app and readme

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Twitter Clone <span>🚀🔥</span></h1>
+
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+
+    </div>
+  );
+}
+
+export default App;
