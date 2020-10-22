664d08b90d7ceafa56615e3443ffca4c05759b98
NashTheCoder <naijeria@gmail.com>
2020-10-22 18:02:44 +0300

    added SidebarOption css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from '@material-ui/icons/Search';
-import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import TurnedInNotIcon from '@material-ui/icons/TurnedInNot';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import SidebarOption from './SidebarOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
+
 
 function Sidebar() {
     return (
@@ -16,17 +20,19 @@ function Sidebar() {
         {/* Twitter icon */}
         <TwitterIcon />
 
-    
-        <SidebarOption Icon={HomeIcon} text='Home'/>
-        <SidebarOption Icon={SearchIcon} text='Explore'/>
-        <SidebarOption Icon={NotificationsNoneIcon} text='Notifications'/>
-        <SidebarOption Icon={MailOutlineIcon} text='Messages'/>
-        <SidebarOption Icon={TurnedInNotIcon} text='Bookmarks'/>
-        <SidebarOption Icon={HomeIcon} text='List'/>
-        <SidebarOption Icon={PersonOutlineIcon} text='Profile'/>
-        <SidebarOption Icon={HomeIcon} text='More'/>
+        <SidebarOption active Icon={HomeIcon} text="Home" />
+        <SidebarOption Icon={SearchIcon} text="Explore" />
+        <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+        <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+        <SidebarOption Icon={ListAltIcon} text="Lists" />
+        <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+        <SidebarOption Icon={MoreHorizIcon} text="More" />
 
         {/* Button -> Tweet */}
+        <Button variant="outlined" className="sidebar__tweet" fullWidth>
+            Tweet
+        </Button>
             
         </div>
     )
