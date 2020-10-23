ce9c06058d2714ee84334c56e69b90d7446dc9fe
NashTheCoder <naijeria@gmail.com>
2020-10-23 12:23:07 +0300

    completed tweetBox styling

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './TweetBox.css';
+import { Avatar, Button, Input } from '@material-ui/core';
 
 function TweetBox() {
     return (
-        <div>
-            
+        <div className='tweetBox'>
+            <form>
+                <div className='tweetBox__input'>
+                    <Avatar src='https://avatars1.githubusercontent.com/u/15178823?s=460&u=37b7e0864e67a10698025f3abc5c12a287fa1f53&v=4'/>
+                    <input placeholder="What's happening?" type='text'/>
+                    <input className='tweetBox__imageInput' placeholder="Optional: Enter image URL" type='text'/>              
+                </div>
+                <Button className='tweetBox__tweetButton'>
+                    Tweet
+                </Button>
+            </form>
         </div>
     )
 }
