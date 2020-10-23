7dae34eec7da1eb17c7981cc273332a62cbdc2b1
NashTheCoder <naijeria@gmail.com>
2020-10-23 22:19:37 +0300

    added widget code

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Widgets.css';
 import SearchIcon from '@material-ui/icons/Search';
+import { TwitterTimelineEmbed,TwitterShareButton,TwitterTweetEmbed } from 'react-twitter-embed';
 
 function Widgets() {
     return (
         <div className='widgets'>
             <div className='widgets__input'>
                 <SearchIcon className='widgets_searchIcon'/>
+                <input placeholder='Search Twitter' type='text'/>
+            </div>
+            <div className='widgets__widgetContainer'>
+                <h2>What's happening</h2>
+
+                <TwitterTweetEmbed />
+
+                <TwitterTimelineEmbed />
+
+                <TwitterShareButton />
 
             </div>
             
