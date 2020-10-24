a12e068e1d5ce0e863154ca9196a3c6f84c0b4d9
NashTheCoder <naijeria@gmail.com>
2020-10-24 23:52:17 +0300

    added firebase config file and installed firebase

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Widgets.css';
 import SearchIcon from '@material-ui/icons/Search';
-import { TwitterTimelineEmbed,TwitterShareButton,TwitterTweetEmbed } from 'react-twitter-embed';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
 
 function Widgets() {
     return (
