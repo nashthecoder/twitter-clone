c48700cf0ee428dd444ee40ad6e80cfa11959986
NashTheCoder <naijeria@gmail.com>
2020-11-16 00:35:06 +0300

    created build folder

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import firebase from "firebase";
-import 'firebase/firestore';
-import 'firebase/auth';
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
